73a0a9cb8b0af25c0292f14c9cd190633750f8f6
localadmin <markus.mueller@sib.swiss>
2023-06-27 17:37:58 +0200

    NeoRanking first commit

--- count_peptides.sh
+++ count_peptides.sh
@@ -1,43 +1,45 @@
+source configure.sh
+
 echo "Mutation NCI CD8 SNV"
-grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation TESLA CD8 SNV"
-grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation HiTIDE CD8 SNV"
-grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation NCI negative SNV"
-grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation TESLA negative SNV"
-grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation HiTIDE negative SNV"
-grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation NCI not_tested SNV"
-grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation TESLA not_tested SNV"
-grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Mutation HiTIDE not_tested SNV"
-grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 
 echo "Neopep NCI CD8 SNV"
-grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep TESLA CD8 SNV"
-grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep HiTIDE CD8 SNV"
-grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]CD8[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep NCI CD8 SNV"
-grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep TESLA CD8 SNV"
-grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep HiTIDE CD8 SNV"
-grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]negative[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep NCI CD8 SNV"
-grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]NCI[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep TESLA CD8 SNV"
-grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]TESLA[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 echo "Neopep HiTIDE CD8 SNV"
-grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]HiTIDE[[:space:]].*[[:space:]]not_tested[[:space:]].*[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 
 echo "Mutation SNV"
-grep -cE "[[:space:]]SNV[[:space:]]" ../paper/data/Mutation_data.txt 
+grep -cE "[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Mutation_data.txt 
 echo "Neopep SNV"
-grep -cE "[[:space:]]SNV[[:space:]]" ../paper/data/Neopep_data.txt 
+grep -cE "[[:space:]]SNV[[:space:]]" $NEORANKING_RESOURCE/data/Neopep_data.txt 
 

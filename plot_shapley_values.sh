c117d29fc4f740b12c3c530807f69242400ac53d
localadmin <markus.mueller@sib.swiss>
2023-08-08 22:00:54 +0200

    NeoRanking first commit

--- plot_shapley_values.sh
+++
@@ -1,8 +0,0 @@
-source configure.sh
-CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotShapleyValues.py -c LR_paper_data*.sav -tr NCI_train -p 4350 -pept KTYQGSYGFRR -pt neopep -ft pdf -fn Figure_3H -las 30 -tis 23 -tts 25 -les 20"
-echo $CMD
-eval $CMD
-
-CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotShapleyValues.py -c LR_paper_data*.sav -tr NCI_train -p 4324 -pept DRNIFRHSVV -pt neopep -ft pdf -fn Figure_3I -las 30 -tis 23 -tts 25 -les 20"
-echo $CMD
-eval $CMD

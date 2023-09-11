36a3345f940c8aebb98dd456f71ebf48724ce253
localadmin <markus.mueller@sib.swiss>
2023-09-11 19:12:01 +0200

    NeoRanking first commit

--- plot_figure_4.sh
+++ plot_figure_4.sh
@@ -1,7 +1,6 @@
 source configure.sh
 
- plot Gartner comparison
-CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotClassifierResults.py -fn Figure_4A -re LR_*_long_*test.txt XGBoost_*_long_*test.txt Voting_classifier_long_0.50_test.txt -pt mutation -g \"{'TESLA':'TESLA','HiTIDE':'HiTIDE','NCI_test':'NCI-test','NCI_train':'NCI-train'}\" -cln \"{0:'LR',1:'XGBoost',2:'Voting'}\" -a 0.02 -rot 30 -las 30 -tis 18 -les 15 -sr -ga -o \"Gartner et al.,LR,XGBoost,Voting\" -ds NCI_test -plt topn_counts"
+CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotClassifierResults.py -fn Figure_4A -re LR_*_long_*test.txt XGBoost_*_long_*test.txt Voting_classifier_0.50_long_test.txt -pt mutation -g \"{'TESLA':'TESLA','HiTIDE':'HiTIDE','NCI_test':'NCI-test','NCI_train':'NCI-train'}\" -cln \"{0:'LR',1:'XGBoost',2:'Voting'}\" -a 0.02 -rot 30 -las 30 -tis 18 -les 15 -sr -ga -o \"Gartner et al.,LR,XGBoost,Voting\" -ds NCI_test -plt topn_counts"
 echo $CMD
 eval $CMD
 
@@ -9,8 +8,8 @@ CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotShapleyImportance.py 
 echo $CMD
 eval $CMD
 
-feat_imp_file_mut="/home/localadmin/Priorization/paper/plots/Figure_4D.txt"
-feat_imp_file_neopep="/home/localadmin/Priorization/paper/plots/Figure_3G.txt"
+feat_imp_file_mut="${NEORANKING_RESOURCE}/plots/Figure_4B.txt"
+feat_imp_file_neopep="${NEORANKING_RESOURCE}/plots/Figure_3G.txt"
 CMD="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotShapImpRankChange.py -fn Figure_4C -fin ${feat_imp_file_neopep} -fim ${feat_imp_file_mut}"
 echo $CMD
 eval $CMD

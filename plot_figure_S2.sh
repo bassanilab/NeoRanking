66f024f5b2fa22d9229ef37ef7a9881bc493612b
localadmin <markus.mueller@sib.swiss>
2023-09-14 12:02:50 +0200

    NeoRanking first commit

--- plot_figure_S2.sh
+++ plot_figure_S2.sh
@@ -1,32 +1,32 @@
 source configure.sh
 
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeaturePairs.py -fn Figure_S2A -pt neopep -ds NCI -ft pdf -fpair mutant_rank,TAP_score -fih 25 -fiw 25 -tis 13 -las 20 -les 15 -rot 40 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeaturePairs.py -fn Figure_S2A -pt neopep -ds NCI -ft pdf -fpair mutant_rank,TAP_score -fih 25 -fiw 25 -tis 13 -las 20 -les 15 -rot 40 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeaturePairs.py -fn Figure_S2B -pt neopep -ds NCI -ft pdf -fpair mutant_rank,mut_netchop_score_ct -fih 25 -fiw 25 -tis 13 -las 20 -les 15 -rot 40 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeaturePairs.py -fn Figure_S2B -pt neopep -ds NCI -ft pdf -fpair mutant_rank,mut_netchop_score_ct -fih 25 -fiw 25 -tis 13 -las 20 -les 15 -rot 40 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeaturePairs.py -fn Figure_S2C -pt neopep -ds NCI -ft pdf -fpair mutant_rank,mut_Rank_Stab -fih 25 -fiw 25 -tis 13 -las 20 -les 15 -rot 40 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeaturePairs.py -fn Figure_S2C -pt neopep -ds NCI -ft pdf -fpair mutant_rank,mut_Rank_Stab -fih 25 -fiw 25 -tis 13 -las 20 -les 15 -rot 40 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2D -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mutant_rank -fih 5 -fiw 5 -rot 45 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2D -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mutant_rank -fih 5 -fiw 5 -rot 45 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2E -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f COUNT_MUT_RANK_CI_MIXMHC -fih 5 -fiw 5 -rot 0 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2E -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f COUNT_MUT_RANK_CI_MIXMHC -fih 5 -fiw 5 -rot 0 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2F -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mutant_rank_PRIME -fih 5 -fiw 5 -rot 45 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2F -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mutant_rank_PRIME -fih 5 -fiw 5 -rot 45 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2G -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f DAI_MixMHC -fih 5 -fiw 5 -rot 0 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2G -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f DAI_MixMHC -fih 5 -fiw 5 -rot 0 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2H -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mut_is_binding_pos -fih 5 -fiw 5 -rot 0 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2H -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mut_is_binding_pos -fih 5 -fiw 5 -rot 0 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2I -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f pep_mut_start_10 -fih 5 -fiw 5 -rot 0 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2I -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f pep_mut_start_10 -fih 5 -fiw 5 -rot 0 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2J -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f seq_len -fih 5 -fiw 5 -rot 0 -nt -o 8 9 10 11 12"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S2J -pt neopep -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f seq_len -fih 5 -fiw 5 -rot 0 -nt -o 8 9 10 11 12"
 echo $cmd
 eval $cmd
--- plot_figure_S3.sh
+++ plot_figure_S3.sh
@@ -1,24 +1,24 @@
 source configure.sh
 
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3A -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f rnaseq_TPM -fih 5 -fiw 5 -rot 45 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3A -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f rnaseq_TPM -fih 5 -fiw 5 -rot 45 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3B -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f rnaseq_alt_support -fih 5 -fiw 5 -rot 45 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3B -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f rnaseq_alt_support -fih 5 -fiw 5 -rot 45 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3C -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f bestWTPeptideCount_I -fih 5 -fiw 5 -rot 45 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3C -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f bestWTPeptideCount_I -fih 5 -fiw 5 -rot 45 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3E -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f bestWTMatchScore_I -fih 5 -fiw 5 -rot 45 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3E -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f bestWTMatchScore_I -fih 5 -fiw 5 -rot 45 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3G -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f CSCAPE_score -fih 5 -fiw 5 -rot 0 -nt"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3G -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f CSCAPE_score -fih 5 -fiw 5 -rot 0 -nt"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3H -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mutation_driver_statement_Intogen -fih 5 -fiw 5 -rot 60 -nt -ena"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3H -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f mutation_driver_statement_Intogen -fih 5 -fiw 5 -rot 60 -nt -ena"
 echo $cmd
 eval $cmd
-cmd="PYTHONPATH=/home/localadmin/Priorization/paper_code python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3I -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f nb_same_mutation_Intogen -fih 5 -fiw 5 -rot 45 -nt -log"
+cmd="PYTHONPATH=$NEORANKING_CODE python3 Scripts/paper/PlotFeatureHistos.py -fn Figure_S3I -pt mutation -ds NCI -ds TESLA -ds HiTIDE -ft pdf -f nb_same_mutation_Intogen -fih 5 -fiw 5 -rot 45 -nt -log"
 echo $cmd
 eval $cmd
 
